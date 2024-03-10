7130b2d645fd774f06f97342e04d6e881673f7b9
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-10 19:45:42 +0600

    "modal and close button done"

--- src/app/components/Pizza.js
+++ src/app/components/Pizza.js
@@ -2,6 +2,7 @@
 import Modal from "react-modal";
 import Image from "next/image";
 import { useState } from "react";
+import { IoCloseOutline } from "react-icons/io5";
 
 Modal.setAppElement("body");
 //style
@@ -67,8 +68,13 @@ const Pizza = ({ pizza }) => {
           style={modalStyles}
           onRequestClose={closeModal}
           contentLabel="Pizza Modal"
+          className={
+            "bg-white w-full h-full lg:max-w-[900px] lg:max-h-[600px] lg:rounded-[30px] lg:fixed lg:top-[50%] lg:left-[50%] lg:translate-x-[-50%] lg:translate-y-[-50%] outline-none"
+          }
         >
-          Modal
+          <div onClick={closeModal} className="absolute z-30 right-2 top-2 hover:scale-110 duration-200 cursor-pointer">
+            <IoCloseOutline className="text-4xl text-orange" />
+          </div>
         </Modal>
       )}
     </div>
