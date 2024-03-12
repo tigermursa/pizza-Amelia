98118511bbc08f50e48c0de0a23276cb41e3b54b
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-13 01:27:06 +0600

    "taking break"

--- src/app/components/Banner.js
+++ src/app/components/Banner.js
@@ -1,5 +1,13 @@
+import { MouseParallaxContainer } from "react-parallax-mouse";
+
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <section className="bg-primary bg-pattern lg:min-h-[768px] pt-16 lg:pt-16">
+      <div className="container mx-auto min-h-[768px] flex items-center ">
+        <MouseParallaxContainer></MouseParallaxContainer>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
