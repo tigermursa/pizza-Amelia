663a9042599b4e9cbf1e5e61f39f9796f950ea54
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-14 19:49:23 +0600

    "working on the banner page"

--- src/app/components/Banner.js
+++ src/app/components/Banner.js
@@ -6,6 +6,7 @@ import { MouseParallaxContainer } from "react-parallax-mouse";
 
 const Banner = () => {
   return (
+    // working on the banner page
     <section className="bg-primary bg-pattern lg:min-h-[768px] pt-16 lg:pt-16">
       <div className="container mx-auto min-h-[768px] flex items-center ">
         <MouseParallaxContainer></MouseParallaxContainer>
