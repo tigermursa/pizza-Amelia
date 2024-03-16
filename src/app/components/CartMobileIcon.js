2c5acbc46ac6c0325cecefe24899f7e91cc74561
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-16 22:25:28 +0600

    "done for today"

--- src/app/components/CartMobileIcon.js
+++ src/app/components/CartMobileIcon.js
@@ -1,8 +1,13 @@
+import { useContext } from "react";
 import { BsHandbagFill } from "react-icons/bs";
 
 const CartMobileIcon = () => {
+  const { isOpen, setIsOpen } = useContext();
   return (
-    <div className="bg-tertiary w-[72px] h-[72px] rounded-full flex justify-center items-center text-white cursor-pointer fixed right-[10%] bottom-[5%] z-20">
+    <div
+      onClick={() => setIsOpen(!isOpen)}
+      className="bg-tertiary w-[72px] h-[72px] rounded-full flex justify-center items-center text-white cursor-pointer fixed right-[10%] bottom-[5%] z-20"
+    >
       <BsHandbagFill className="text-4xl" />
       <span className="absolute text-white bottom-3 right-4 gradient w-5 h-5 flex justify-center items-center rounded-full font-robotoCondensed text-[13px]">
         3
--- src/app/context/CartContext.js
+++ src/app/context/CartContext.js
@@ -1,11 +1,13 @@
-import { createContext } from "react";
+"use client";
+import { createContext, useState } from "react";
 
 // cart context
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <CartContext.Provider value={"cart context"}>
+    <CartContext.Provider value={(isOpen, setIsOpen)}>
       {children}
     </CartContext.Provider>
   );
