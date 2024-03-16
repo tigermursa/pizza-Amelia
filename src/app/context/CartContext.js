66d20da90a3cb31f8dae2974621f7bc93c4eb8bb
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-16 22:18:34 +0600

    "created cart provider"

--- src/app/context/CartContext.js
+++ src/app/context/CartContext.js
@@ -1,1 +1,14 @@
+import { createContext } from "react";
+
 // cart context
+export const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+  return (
+    <CartContext.Provider value={"cart context"}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
