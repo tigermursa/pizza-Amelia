eb15b84b9620a81fe3c8542afaea7329bbd0c634
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-16 22:15:08 +0600

    "done the design of the mobile cart"

--- src/app/components/CartMobileIcon.js
+++ src/app/components/CartMobileIcon.js
@@ -2,8 +2,11 @@ import { BsHandbagFill } from "react-icons/bs";
 
 const CartMobileIcon = () => {
   return (
-    <div className="bg-tertiary w-[72px] h-[72px] rounded-full flex justify-center items-center text-white cursor-pointer fixed ">
-      <BsHandbagFill />
+    <div className="bg-tertiary w-[72px] h-[72px] rounded-full flex justify-center items-center text-white cursor-pointer fixed right-[10%] bottom-[5%] z-20">
+      <BsHandbagFill className="text-4xl" />
+      <span className="absolute text-white bottom-3 right-4 gradient w-5 h-5 flex justify-center items-center rounded-full font-robotoCondensed text-[13px]">
+        3
+      </span>
     </div>
   );
 };
