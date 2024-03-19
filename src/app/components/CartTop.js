6efcce000ab67c4b7e2575c3f6056a2627a4110f
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-19 20:30:54 +0600

    "working in cart desktop now"

--- src/app/components/CartTop.js
+++ src/app/components/CartTop.js
@@ -7,7 +7,7 @@ const CartTop = () => {
   const { setIsOpen } = useContext(CartContext);
   return (
     <div className="w-full h-20 border-b flex items-center justify-between px-10 ">
-      <div className="text-font-semibold">Hello there I am the boss</div>
+      <div className="text-font-semibold">shopping bag (3)</div>
       <div onClick={() => setIsOpen(false)} className="cursor-pointer group">
         <IoCloseOutline className="text-3xl group-hover:scale-110 duration-300 transition-all" />
       </div>
