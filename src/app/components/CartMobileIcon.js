a1ec95e04db93516819e10cdad6a3d6788330b22
Mursalin Hossain <mursalin2311@gmail.com>
2024-04-04 23:31:55 +0600

    "going to work with checkout details"

--- src/app/components/CartMobileIcon.js
+++ src/app/components/CartMobileIcon.js
@@ -5,7 +5,7 @@ import { BsHandbagFill } from "react-icons/bs";
 import { CartContext } from "../context/CartContext";
 
 const CartMobileIcon = () => {
-  const { isOpen, setIsOpen } = useContext(CartContext);
+  const { isOpen, setIsOpen, itemAmount } = useContext(CartContext);
   return (
     <div
       onClick={() => setIsOpen(!isOpen)}
@@ -13,7 +13,7 @@ const CartMobileIcon = () => {
     >
       <BsHandbagFill className="text-4xl" />
       <span className="absolute text-white bottom-3 right-4 gradient w-5 h-5 flex justify-center items-center rounded-full font-robotoCondensed text-[13px]">
-        3
+        {itemAmount}
       </span>
     </div>
   );
